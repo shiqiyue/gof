8b18213f6951369521d05915fc6d9ad68b28be37
shiqiyue <835802084@qq.com>
2022-12-08 10:43:19 +0800

    fix: is_login.go bug

--- gqlgens/directive/is_login.go
+++ gqlgens/directive/is_login.go
@@ -3,15 +3,14 @@ package directive
 import (
 	"context"
 	"errors"
-	"gitee.com/shiqiyue/go-admin/internal/pkg/auths"
 	"github.com/99designs/gqlgen/graphql"
 )
 
-// 需要登录
-func NeedLogin(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
-	userId := auths.GetUserId(ctx)
-	if userId == nil {
+func NeedLogin(isLoginFunc func(ctx context.Context) bool) func(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
+	return func(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
+		if isLoginFunc(ctx) {
+			return next(ctx)
+		}
 		return nil, errors.New("请先登录")
 	}
-	return next(ctx)
 }
